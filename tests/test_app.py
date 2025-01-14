de3c0ae9f1cbd8a5b8670572edf748963a0fd23d
Scott Olesen <ulp7@cdc.gov>
2025-01-14 13:12:47 -0500

    ignore altair theme warning (#73)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
+import pytest
 from streamlit.testing.v1 import AppTest
 
 
+@pytest.mark.filterwarnings(
+    r"ignore:\s+Deprecated since `altair=5.5.0`. Use altair.theme instead."
+)
 def test_app():
     # Cf. https://docs.streamlit.io/develop/api-reference/app-testing
     at = AppTest.from_file("ngm/app.py")
